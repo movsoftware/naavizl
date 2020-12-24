587ccddbce026782fa07734819d1239e5815adbe
pendleto <pendleto@movsoftware.com>
2020-12-24 13:41:12 -0500

    Do not collect flows to the flow collector server

--- naavizlclientdocker/run.sh
+++ naavizlclientdocker/run.sh
@@ -6,7 +6,7 @@
 
 while true
     do
-        yaf --out $SILK_HOST --ipfix-port $SILK_PORT --ipfix tcp --live pcap --in $IFACE --verbose --silk
+        yaf --out $SILK_HOST --ipfix-port $SILK_PORT --ipfix tcp --live pcap --in $IFACE --verbose --silk  --filter "ip host not $SILK_HOST"
         sleep 1
     done
 done
